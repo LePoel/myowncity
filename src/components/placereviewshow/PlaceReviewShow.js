318bda678ec4ac920b849982854a88d3d71c4913
Paul Pier <paulpier@outlook.de>
2024-03-27 11:58:26 +0100

    Cards little bit updatet

--- src/components/placereviewshow/PlaceReviewShow.js
+++ src/components/placereviewshow/PlaceReviewShow.js
@@ -1,26 +1,24 @@
+import Card from 'react-bootstrap/Card';
+
 function PlaceReviewShow({ city, postalcode, street, reviews, ratings, images }) {
+    const totalStars = ratings.reduce((acc, curr) => acc + curr.stars, 0);
+    const averageRating = totalStars / ratings.length;
     return (
-        <div>
-            <h2>Address: {street}, {postalcode} {city}</h2>
-            <h2>Ratings:</h2>
-            <ul>
-                {ratings.map((rating, index) => (
-                    <li key={index}>{rating.stars}</li>
-                ))}
-            </ul>
-            <h2>Reviews:</h2>
-            <ul>
-                {reviews.map((review, index) => (
-                    <li key={index}>{review.comment}</li>
-                ))}
-            </ul>
-            <h2>Pictures:</h2>
-            <ul>
-                {images.map((image, index) => (
-                    <img src={image.imageUrl} className="img-fluid" alt="Place picture" key={index} />
-                ))}
-            </ul>
-        </div>
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={images.length > 0 ? images[0].imageUrl : ''} {...(images.length > 0 && { alt: "Picture of a place" })}/>
+            <Card.Body>
+                <Card.Title>{street.charAt(0).toUpperCase() + street.slice(1)}</Card.Title>
+                <Card.Text>
+                    <h2>Average Rating: {averageRating}</h2>
+                    <h2>Reviews:</h2>
+                    <ul>
+                        {reviews.map((review, index) => (
+                            <li key={index}>{review.comment}</li>
+                        ))}
+                    </ul>
+                </Card.Text>
+            </Card.Body>
+        </Card>
     );
 }
 
