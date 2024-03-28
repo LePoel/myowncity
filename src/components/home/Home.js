e0fe0173f709143931e27b89697f7387510c3c75
Paul Pier <paulpier@outlook.de>
2024-03-28 15:06:26 +0100

    only one item in carousel and add default image

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,11 @@
 import Map from "../map/Map";
-import { db } from "../../config/firebase";
-import React, { useEffect, useState } from "react";
-import { getDocs, collection, query } from "firebase/firestore"
+import {db} from "../../config/firebase";
+import React, {useEffect, useState} from "react";
+import {getDocs, collection, query} from "firebase/firestore"
 import PlaceReviewShow from "../placereviewshow/PlaceReviewShow";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import Carousel from "react-bootstrap/Carousel";
+
 function Home() {
     const [placeReviews, setPlaceReviews] = useState([]);
 
@@ -44,7 +45,7 @@ function Home() {
                     const imagesData = imagesCollection.docs.map(doc => doc.data());
 
 
-                    return { ...placereview, ratings: ratingsData, reviews: reviewData, imageRefs: imagesData };
+                    return {...placereview, ratings: ratingsData, reviews: reviewData, imageRefs: imagesData};
                 }));
             setPlaceReviews(placeReviewWithRatingsAndReviewsAndPictures);
         } catch (err) {
@@ -56,34 +57,30 @@ function Home() {
         getPlaceReviews();
     }, []);
 
-    const chunks = placeReviews.reduce((acc, _, index, array) => {
-        if (index % 3 === 0) {
-            acc.push(array.slice(index, index + 3));
-        }
-        return acc;
-    }, []);
-
     return (
         <div className="home container-fluid m-0 p-0">
             <div className="row m-3 m-md-5 pt-3 pt-md-5">
                 <div className="col-md-5 mb-4 mt-md-4 p-3">
                     <div className="row">
-                        <div className="col-xs-12 mb-md-3 text-center align-content-center text-primary fs-5 " id="einleitung">
+                        <div className="col-xs-12 mb-md-3 text-center align-content-center text-primary fs-5 "
+                             id="einleitung">
                             <p className="m-0">Navigate with <span className="fw-bold">care</span>, </p>
                             <p className="m-0">live with <span className="fw-bold">confidence</span>!</p>
                         </div>
                         <div className="col-xs-12 text-center p-3" id="erläuterung">
-                            <p id="erklärung">MyCity operates an interactive map that shows places in your neighborhood that have been rated by our community.</p>
+                            <p id="erklärung">MyCity operates an interactive map that shows places in your neighborhood
+                                that have been rated by our community.</p>
                             <p className="fw-semibold">Came across a place you want to tell our community about?</p>
                         </div>
                         <div className="col text-center pt-md-3">
-                            <Link to="/reportplace" className="btn btn-primary btn-lg rounded-5" role="button">Report a Place</Link>
+                            <Link to="/reportplace" className="btn btn-primary btn-lg rounded-5" role="button">Report a
+                                Place</Link>
                         </div>
                     </div>
                 </div>
                 <div className="col-md-7 m-0 p-0">
                     <div className="bg-light rounded-3 p-3">
-                        <Map placeReviews={placeReviews} />
+                        <Map placeReviews={placeReviews}/>
                     </div>
                 </div>
             </div>
@@ -91,20 +88,18 @@ function Home() {
                 <div className="col bg-light rounded-3 mt-3 mt-md-0 mb-3 p-3">
                     <h4 className="fw-medium">Newest Community Reviews</h4>
                     <Carousel indicators={false}>
-                        {chunks.map((chunk, index) => (
-                            <Carousel.Item key={index}>
+                        {placeReviews.map((placeReview, innerIndex) => (
+                            <Carousel.Item>
                                 <div className="d-flex">
-                                    {chunk.map((placeReview, innerIndex) => (
-                                        <PlaceReviewShow
-                                            key={innerIndex}
-                                            city={placeReview.city}
-                                            postalcode={placeReview.postalcode}
-                                            street={placeReview.street}
-                                            reviews={placeReview.reviews}
-                                            ratings={placeReview.ratings}
-                                            images={placeReview.imageRefs}
-                                        />
-                                    ))}
+                                    <PlaceReviewShow
+                                        key={innerIndex}
+                                        city={placeReview.city}
+                                        postalcode={placeReview.postalcode}
+                                        street={placeReview.street}
+                                        reviews={placeReview.reviews}
+                                        ratings={placeReview.ratings}
+                                        images={placeReview.imageRefs}
+                                    />
                                 </div>
                             </Carousel.Item>
                         ))}
--- src/components/placereviewshow/PlaceReviewShow.js
+++ src/components/placereviewshow/PlaceReviewShow.js
@@ -1,8 +1,9 @@
 import Card from 'react-bootstrap/Card';
 import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import defaultImageUrl from './images/defaultImage.png';
 
-function StarRating({ rating }) {
+function StarRating({rating}) {
     const numStars = 5;
     const fullStars = Math.floor(rating);
     const halfStars = Math.ceil(rating - fullStars);
@@ -23,29 +24,38 @@ function StarRating({ rating }) {
 }
 
 
-function PlaceReviewShow({ city, postalcode, street, reviews, ratings, images }) {
+function PlaceReviewShow({city, postalcode, street, reviews, ratings, images}) {
     const totalStars = ratings.reduce((acc, curr) => acc + curr.stars, 0);
     const averageRating = totalStars / ratings.length;
     return (
         <div className="row">
             <div className="col-6">
-                <Card border="grey" className="m-2" style={{ width: '18rem' }}>
+                <Card border="grey" className="m-2" style={{width: '18rem'}}>
 
-                    <Carousel>
-                        {images.map((image, index) => (
-                            <Carousel.Item key={index}>
-                                <Card.Img variant="top"
-                                    src={image.imageUrl}
-                                    alt={"Picture of a place"}
-                                />
-                            </Carousel.Item>
-                        ))}
-                    </Carousel>
+                    {images.length > 0 ? (
+                        <Carousel>
+                            {images.map((image, index) => (
+                                <Carousel.Item key={index}>
+                                    <Card.Img variant="top"
+                                              src={image.imageUrl}
+                                              alt={"Picture of a place"}
+                                    />
+                                </Carousel.Item>
+                            ))}
+                        </Carousel>
+                    ) : (
+                        <Card.Img variant="top"
+                                  src={defaultImageUrl}
+                                  alt={"Default Picture"}
+                        />
+                    )}
+
                     <Card.Body>
-                        <Card.Title className="text-secondary fs-6">{street.charAt(0).toUpperCase() + street.slice(1)}</Card.Title>
+                        <Card.Title
+                            className="text-secondary fs-6">{street.charAt(0).toUpperCase() + street.slice(1)}</Card.Title>
                         <Card.Text>
                             <h5>Average Rating </h5>
-                            <div className="mb-3"><StarRating rating={averageRating} /></div>
+                            <div className="mb-3"><StarRating rating={averageRating}/></div>
                             <h5>Review comments</h5>
                             {reviews.map((review, index) => (
                                 <div key={index} className="card shadow-sm bg-light mb-3">
